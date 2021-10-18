2e97c1e1301b405377985ab910f5ec25a487c19d
bibaHH <bibahallil@gmail.com>
2021-10-18 10:25:57 +0200

    final

--- tp2/formulaire.js
+++ tp2/formulaire.js
@@ -3,29 +3,50 @@ function validation ()
 document.getElementById("resultat").setAttribute("hidden",'hidden');
 document.getElementById("error").setAttribute("hidden",'hidden');
 
-    if (document.getElementById("nom").value.length < 5 ) { 
+if (document.getElementById("nom").value === "" ) { 
+    document.getElementById("error").innerHTML = " La saisie du nom est obligatoire  !";
+    document.getElementById("error").removeAttribute("hidden");
+}    
+else if (document.getElementById("nom").value.length < 5 ) { 
         document.getElementById("error").innerHTML = "Le nom doit contenir au moins 5 caractère !";
         document.getElementById("error").removeAttribute("hidden");
     }  
-
-     
-     else if(document.getElementById("nom").value.length < 5 ) { 
+else if (document.getElementById("nom").value.length > 20 ) { 
+        document.getElementById("error").innerHTML = "Le nom doit contenir au max 20 caractère !";
+        document.getElementById("error").removeAttribute("hidden");
+    }  
+else if (document.getElementById("prenom").value === "" ) { 
+        document.getElementById("error").innerHTML = " La saisie du prenom est obligatoire  !";
+        document.getElementById("error").removeAttribute("hidden");
+    }    
+else if(document.getElementById("prenom").value.length < 5 ) { 
         document.getElementById("error").innerHTML = "Le prenom doit contenir au moins 5 caractère !";
         document.getElementById("error").removeAttribute("hidden"); 
     }    
-      
+    else if (document.getElementById("prenom").value.length > 20 ) { 
+        document.getElementById("error").innerHTML = "Le prénom doit contenir au max 20 caractère !";
+        document.getElementById("error").removeAttribute("hidden");
+    } 
+    else if (document.getElementById("adresse").value === "" ) { 
+        document.getElementById("error").innerHTML = " La saisie du prenom est obligatoire  !";
+        document.getElementById("error").removeAttribute("hidden");
+    } 
      else if(document.getElementById("adresse").value.length < 5 ) { 
         document.getElementById("error").innerHTML = "L'adresse doit contenir au moins 5 caractère !";
         document.getElementById("error").removeAttribute("hidden");
     }    
-     
-     else if(document.getElementById("mail").value.length < 5 ) { 
+    else if (document.getElementById("adresse").value.length > 50 ) { 
+        document.getElementById("error").innerHTML = "L'adresse doit contenir au max 50 caractère !";
+        document.getElementById("error").removeAttribute("hidden");
+    }
+     else if(document.getElementById("mail").value === "") { 
         document.getElementById("error").innerHTML = "Le mail doit contenir au moins 5 caractère !";
         document.getElementById("error").removeAttribute("hidden");
     }    
+  
      else {
         document.getElementById("resultat").removeAttribute("hidden");
-        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#nom").value;
+        document.getElementById("resultat").innerHTML = "Bienvenue " + document.querySelector("#prenom").value;
 
      }
 
